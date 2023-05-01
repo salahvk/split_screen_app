7912847ef4d260a85db4d96344b8e1a246f32f5a
mo <salahkoyilandy@gmail.com>
2023-05-01 09:48:04 +0530

    minor updates

--- lib/core/utils/change_url.dart
+++ lib/core/utils/change_url.dart
@@ -1,38 +1,38 @@
-import 'package:youtube_explode_dart/youtube_explode_dart.dart';
+// import 'package:youtube_explode_dart/youtube_explode_dart.dart';
 
-Future<String> getVideoUrl(url) async {
-  var yt = YoutubeExplode();
-  var manifest = await yt.videos.streamsClient.getManifest(url);
-  String videoUrl;
-  // var s = manifest.videoOnly.getAllVideoQualitiesLabel();
-  // var s1 = manifest.videoOnly.getAllVideoQualities();
-  // if(manifest.videoOnly.getAllVideoQualities().contains(VideoQuality.high1080)){
+// Future<String> getVideoUrl(url) async {
+//   var yt = YoutubeExplode();
+//   var manifest = await yt.videos.streamsClient.getManifest(url);
+//   String videoUrl;
+//   // var s = manifest.videoOnly.getAllVideoQualitiesLabel();
+//   // var s1 = manifest.videoOnly.getAllVideoQualities();
+//   // if(manifest.videoOnly.getAllVideoQualities().contains(VideoQuality.high1080)){
 
-  // }
+//   // }
 
-  if (manifest.videoOnly
-      .getAllVideoQualities()
-      .contains(VideoQuality.high720)) {
-    var video = manifest.videoOnly.firstWhere(
-      (stream) => stream.videoQuality == VideoQuality.high720,
-    );
-    videoUrl = video.url.toString();
-  } else {
-    videoUrl = manifest.videoOnly.last.url.toString();
-  }
-  // var s2 = manifest.videoOnly.bestQuality;
-  // print("________________");
-  // print(s1.contains(VideoQuality.high1080));
-  // print(s2.videoQuality);
-  // print(s);
-  // var videog = manifest.videoOnly.sortByVideoQuality();
-  // var video = manifest.videoOnly.sortByVideoQuality().firstWhere((stream) {
-  //   final s = stream.videoQuality;
-  //   print(s);
-  //   return true;
-  // });
-  // print("object");
-  // print(video.url.toString());
-  print(videoUrl);
-  return videoUrl;
-}
+//   if (manifest.videoOnly
+//       .getAllVideoQualities()
+//       .contains(VideoQuality.high720)) {
+//     var video = manifest.videoOnly.firstWhere(
+//       (stream) => stream.videoQuality == VideoQuality.high720,
+//     );
+//     videoUrl = video.url.toString();
+//   } else {
+//     videoUrl = manifest.videoOnly.last.url.toString();
+//   }
+//   // var s2 = manifest.videoOnly.bestQuality;
+//   // print("________________");
+//   // print(s1.contains(VideoQuality.high1080));
+//   // print(s2.videoQuality);
+//   // print(s);
+//   // var videog = manifest.videoOnly.sortByVideoQuality();
+//   // var video = manifest.videoOnly.sortByVideoQuality().firstWhere((stream) {
+//   //   final s = stream.videoQuality;
+//   //   print(s);
+//   //   return true;
+//   // });
+//   // print("object");
+//   // print(video.url.toString());
+//   print(videoUrl);
+//   return videoUrl;
+// }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,12 +9,10 @@ import path_provider_foundation
 import platform_device_id
 import platform_device_id_macos
 import sqflite
-import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   PlatformDeviceIdMacosPlugin.register(with: registry.registrar(forPlugin: "PlatformDeviceIdMacosPlugin"))
   PlatformDeviceIdMacosPlugin.register(with: registry.registrar(forPlugin: "PlatformDeviceIdMacosPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
-  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
